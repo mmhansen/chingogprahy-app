15356499119fb8ae6035a59daa2b7cc38d880dca
mmhansen <hansen.mmb@gmail.com>
2017-02-17 19:27:53 -0500

    very simple pagination

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,14 @@ import React, { PropTypes } from 'react'
 import Intro from './Intro'
 import IntroForm from './IntroForm'
 
-function Home ({ intros, formOpen }) {
+function Home ({ intros, formOpen, nextIntros }) {
   return (
     <div className="container">
       { formOpen && <IntroForm /> }
       {
         intros.map((intro, ix) => <Intro {...intro} key={intro._id} />)
       }
+      <a className="f6 link dim ba ph3 pv2 mb2 dib near-black pointer" onClick={() => { nextIntros() }}>More Intros</a>
     </div>
   )
 }
--- src/containers/FetchIntros.jsx
+++ src/containers/FetchIntros.jsx
@@ -2,16 +2,22 @@ import React, { PropTypes, Component } from 'react'
 import axios from 'axios'
 import { url } from '../config'
 
-export default function(ComposedComponent) {
+const getIntros = (skip = 0) => {
+  return axios.get(url+'/api/intro?skip='+skip)
+}
+
+export default function (ComposedComponent) {
   return class FetchIntroWrapper extends Component {
-    constructor(props) {
+    constructor (props) {
       super(props)
+      this.nextIntros = this.nextIntros.bind(this)
       this.state = {
-        intros: []
+        intros: [],
+        skip: 0
       }
     }
-    componentWillMount() {
-      axios.get(url+'/api/intro')
+    componentWillMount () {
+      getIntros()
         .then(res => {
           this.setState({
             intros: res.data.intros
@@ -21,8 +27,22 @@ export default function(ComposedComponent) {
           console.log(err)
         })
     }
-    render() {
-      return <ComposedComponent {...this.state} {...this.props} />
+    nextIntros () {
+      this.setState({
+        skip: this.state.skip += 10
+      })
+      getIntros(this.state.skip)
+        .then(res => {
+          this.setState({
+            intros: [...this.state.intros, ...res.data.intros]
+          })
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+    render () {
+      return <ComposedComponent {...this.state} {...this.props} nextIntros={this.nextIntros} />
     }
   }
 }
